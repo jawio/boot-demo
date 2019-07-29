4883ccc868b89960b2b8c01bfce1c17487880b42
atofinon <yangjw0922@163.com>
2019-07-29 22:49:13 +0800

    init

---
+++ src/main/java/cn/atofinon/cloud/freshman/FreshmanApplication.java
@@ -0,0 +1,13 @@
+package cn.atofinon.cloud.freshman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/atofinon/cloud/freshman/FreshmanApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.atofinon.cloud.freshman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreshmanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
