3b929ff98102b9118600b0288021a891a41c92f5
yangjw <yangjw@tuya.com>
2021-08-05 11:15:27 +0800

    morris

--- src/main/java/cn/atofinon/cloud/freshman/leetcode/Morris.java
+++ src/main/java/cn/atofinon/cloud/freshman/leetcode/Morris.java
@@ -1,5 +1,8 @@
 package cn.atofinon.cloud.freshman.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author yangjw@tuya.com
  * @Date 2021/7/19 23:37
@@ -80,4 +83,80 @@ public class Morris {
         }
     }
 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.add(root.val);
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.add(root.val);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+
+
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<Integer>();
+//
+//        TreeNode cur = root;
+//        TreeNode mostRight = null;
+//        while (cur != null) {
+//            if (cur.left != null) {
+//                // 左子树的最右子树
+//                mostRight = cur.left;
+//                while (mostRight.right != null && mostRight.right != cur) {
+//                    mostRight = mostRight.right;
+//                }
+//                // 最右子树的右子树为空，则最右子树的右子树指向当前节点，并且当前节点左移
+//                if (mostRight.right == null) {
+//                    mostRight.right = cur;
+//                    cur = cur.left;
+//                } else {
+//                    // 最右子树的右子树指向当前节点，则最右子树指向null，并且当前节点右移
+//                    res.add(cur.val);
+//                    mostRight.right = null;
+//                    cur = cur.right;
+//                }
+//            } else {
+//                res.add(cur.val);
+//                cur = cur.right;
+//            }
+//        }
+//        return res;
+//    }
+
+//    Deque<TreeNode> deque =new LinkedList<TreeNode>();
+
+    //     while(root!=null || !deque.isEmpty()){
+    //         while(root!=null){
+    //             deque.push(root);
+    //             root=root.left;
+    //         }
+    //         root=deque.pop();
+    //         res.add(root.val);
+    //         root=root.right;
+    //     }
+    //     return res;
 }
---
+++ src/main/java/cn/atofinon/cloud/freshman/module/Sample.java
@@ -0,0 +1,15 @@
+package cn.atofinon.cloud.freshman.module;
+
+/**
+ * @Author yangjw@tuya.com
+ * @Date 2021/7/27 21:09
+ * @Descrition
+ */
+
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("hello jdk 11 module demo.");
+    }
+}
